332f172294615c4fa6fe9646c0768758850bf861
Kieran <bmxstreet@gmx.net>
2018-01-12 12:11:41 +0100

    current date and check for empty name

--- add.php
+++ add.php
@@ -7,30 +7,14 @@ $chatId = preg_replace("/[^0-9]/", "", $argv[1]);
 $name = preg_replace("/[^\w]/", '', $argv[2]);
 $chatIdQuery = $chatId . '1' . $config['escapeString'];
 $added = '';
+$email = $chatId;
+$ip = 'telegram';
+$current_date = date('Y-m-d H:i:s');
 
 for ($i = 3; $i <= $argv; $i++) {
   $profileName = $argv[$i];
-  $email = $chatId;
-  $ip = 'telegram';
-
-  $sql = "SELECT `email` FROM `fajournalmon` WHERE `profile`='" . $profileName . "'";
-  $results = mysqli_query($conn, $sql);
-  if ($results == false) {
-    $to = 'admin@kieran.pw';
-    $subject = 'Error journal check';
-    $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
-    $headers = 'From: fajournal@kieran.pw';
-    mail($to, $subject, $txt, $headers);
-    die("sql");
-  }
-  $profileExists = 0;
-  $rows = mysqli_fetch_row($results);
-  if (count($rows) == 1) {
-    $profileExists = 1;
-  }
-
-  if ($profileExists) {
-    $sql = "UPDATE `fajournalmon` SET `name`=CONCAT(name, '" . $name . "|'),`email`= CONCAT(email, '" . $email . '0' . $config['escapeString'] . "'),`time`= CONCAT(time, '" . $current_date . "|'),`ip`= CONCAT(ip, '" . $ip . "|') WHERE `profile`='" . $profileName . "'";
+  if (!empty($profileName)) {
+    $sql = "SELECT `email` FROM `fajournalmon` WHERE `profile`='" . $profileName . "'";
     $results = mysqli_query($conn, $sql);
     if ($results == false) {
       $to = 'admin@kieran.pw';
@@ -40,25 +24,43 @@ for ($i = 3; $i <= $argv; $i++) {
       mail($to, $subject, $txt, $headers);
       die("sql");
     }
-  }
-  else {
-    $sql = "SELECT MAX(id),`b`,`a` FROM `fajournalmon_cookies` WHERE `a` <> ''";
-    $rows = mysqli_fetch_row($results)[0];
-
-    $newest = checkTitle($rows['a'], $rows['b'], $profileName);
-    $sql = "INSERT INTO `fajournalmon`(`profile`, `name`, `email`, `lastTitle`, `time`, `ip`, `lastId`) VALUES ('{$profileName}', '{$name}|', '{$email}0{$config['escapeString']}', '{$newest[0]}','{$current_date}|','{$ip}|', '{$newest[1]}')";
-    $results = mysqli_query($conn, $sql);
-    if ($results == false) {
-      $to = 'admin@kieran.pw';
-      $subject = 'Error journal check';
-      $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
-      $headers = 'From: fajournal@kieran.pw';
-      mail($to, $subject, $txt, $headers);
-      die("sql");
+    $profileExists = 0;
+    $rows = mysqli_fetch_row($results);
+    if (count($rows) == 1) {
+      $profileExists = 1;
     }
-  }
 
-  $added .= ' ' . $argv[$i];
+    if ($profileExists) {
+      $sql = "UPDATE `fajournalmon` SET `name`=CONCAT(name, '" . $name . "|'),`email`= CONCAT(email, '" . $email . '0' . $config['escapeString'] . "'),`time`= CONCAT(time, '" . $current_date . "|'),`ip`= CONCAT(ip, '" . $ip . "|') WHERE `profile`='" . $profileName . "'";
+      $results = mysqli_query($conn, $sql);
+      if ($results == false) {
+        $to = 'admin@kieran.pw';
+        $subject = 'Error journal check';
+        $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
+        $headers = 'From: fajournal@kieran.pw';
+        mail($to, $subject, $txt, $headers);
+        die("sql");
+      }
+    }
+    else {
+      $sql = "SELECT MAX(id),`b`,`a` FROM `fajournalmon_cookies` WHERE `a` <> ''";
+      $rows = mysqli_fetch_row($results)[0];
+
+      $newest = checkTitle($rows['a'], $rows['b'], $profileName);
+      $sql = "INSERT INTO `fajournalmon`(`profile`, `name`, `email`, `lastTitle`, `time`, `ip`, `lastId`) VALUES ('{$profileName}', '{$name}|', '{$email}0{$config['escapeString']}', '{$newest[0]}','{$current_date}|','{$ip}|', '{$newest[1]}')";
+      $results = mysqli_query($conn, $sql);
+      if ($results == false) {
+        $to = 'admin@kieran.pw';
+        $subject = 'Error journal check';
+        $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
+        $headers = 'From: fajournal@kieran.pw';
+        mail($to, $subject, $txt, $headers);
+        die("sql");
+      }
+    }
+
+    $added .= ' ' . $argv[$i];
+  }
 }
 
 echo "Added the following users to your monitor list: " . $added;
