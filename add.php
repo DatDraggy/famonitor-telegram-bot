4d0bd70aa0c81b24ef9d4b736b0193b8eac493fc
Kieran <bmxstreet@gmx.net>
2018-01-12 13:02:32 +0100

    Stuff

--- add.php
+++ add.php
@@ -13,7 +13,7 @@ $current_date = date('Y-m-d H:i:s');
 
 for ($i = 3; $i <= $argv; $i++) {
   if (!empty($argv[$i])) {
-    $profileName = $argv[$i];
+    $profileName = strtolower($argv[$i]);
     $sql = "SELECT `email` FROM `fajournalmon` WHERE `profile`='" . $profileName . "'";
     $results = mysqli_query($conn, $sql);
     if ($results == false) {
@@ -28,54 +28,53 @@ for ($i = 3; $i <= $argv; $i++) {
     $rows = mysqli_fetch_row($results);
     if (count($rows) == 1) {
       $profileExists = 1;
-    }
-    if (strpos($rows[0], $email) !== false) {
-      if (strpos($rows[0], $email . '0' . $config['escapeString']) !== false) {
-        $sql = "UPDATE `fajournalmon` SET `name`=CONCAT(name, '" . $name . "|'),`email`= CONCAT(email, '" . $email . '1' . $config['escapeString'] . "'),`time`= CONCAT(time, '" . $current_date . "|'),`ip`= CONCAT(ip, '" . $ip . "|') WHERE `profile`='" . $profileName . "'";
-        $results = mysqli_query($conn, $sql);
-        if ($results == false) {
-          $to = 'admin@kieran.pw';
-          $subject = 'Error journal check';
-          $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
-          $headers = 'From: fajournal@kieran.pw';
-          mail($to, $subject, $txt, $headers);
-          die("sql");
+      if (strpos($rows[0], $email) !== false) {
+        if (strpos($rows[0], $email . '0' . $config['escapeString']) !== false) {
+          $sql = "UPDATE `fajournalmon` SET `email`=REPLACE(`email`, '{$email}1{$config['escapeString']}', '{$email}0{$config['escapeString']}') WHERE `profile`='{$profileName}'";
+          $results = mysqli_query($conn, $sql);
+          if ($results == false) {
+            $to = 'admin@kieran.pw';
+            $subject = 'Error journal check';
+            $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
+            $headers = 'From: fajournal@kieran.pw';
+            mail($to, $subject, $txt, $headers);
+            die("sql");
+          }
         }
+      }
+    }
+    if ($profileExists) {
+      $sql = "UPDATE `fajournalmon` SET `name`=CONCAT(name, '" . $name . "|'),`email`= CONCAT(email, '" . $email . '1' . $config['escapeString'] . "'),`time`= CONCAT(time, '" . $current_date . "|'),`ip`= CONCAT(ip, '" . $ip . "|') WHERE `profile`='" . $profileName . "'";
+      $results = mysqli_query($conn, $sql);
+      if ($results == false) {
+        $to = 'admin@kieran.pw';
+        $subject = 'Error journal check';
+        $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
+        $headers = 'From: fajournal@kieran.pw';
+        mail($to, $subject, $txt, $headers);
+        die("sql");
       }
     }
     else {
-      if ($profileExists) {
-        $sql = "UPDATE `fajournalmon` SET `name`=CONCAT(name, '" . $name . "|'),`email`= CONCAT(email, '" . $email . '1' . $config['escapeString'] . "'),`time`= CONCAT(time, '" . $current_date . "|'),`ip`= CONCAT(ip, '" . $ip . "|') WHERE `profile`='" . $profileName . "'";
-        $results = mysqli_query($conn, $sql);
-        if ($results == false) {
-          $to = 'admin@kieran.pw';
-          $subject = 'Error journal check';
-          $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
-          $headers = 'From: fajournal@kieran.pw';
-          mail($to, $subject, $txt, $headers);
-          die("sql");
-        }
-      }
-      else {
-        $sql = "SELECT MAX(id),`b`,`a` FROM `fajournalmon_cookies` WHERE `a` <> ''";
-        $rows = mysqli_fetch_row($results)[0];
+      $sql = "SELECT MAX(id),`b`,`a` FROM `fajournalmon_cookies` WHERE `a` <> ''";
+      $rows = mysqli_fetch_row($results)[0];
 
-        $newest = checkTitle($rows['a'], $rows['b'], $profileName);
-        $sql = "INSERT INTO `fajournalmon`(`profile`, `name`, `email`, `lastTitle`, `time`, `ip`, `lastId`) VALUES ('{$profileName}', '{$name}|', '{$email}1{$config['escapeString']}', '{$newest[0]}','{$current_date}|','{$ip}|', '{$newest[1]}')";
-        $results = mysqli_query($conn, $sql);
-        if ($results == false) {
-          $to = 'admin@kieran.pw';
-          $subject = 'Error journal check';
-          $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
-          $headers = 'From: fajournal@kieran.pw';
-          mail($to, $subject, $txt, $headers);
-          die("sql");
-        }
+      $newest = checkTitle($rows['a'], $rows['b'], $profileName);
+      $sql = "INSERT INTO `fajournalmon`(`profile`, `name`, `email`, `lastTitle`, `time`, `ip`, `lastId`) VALUES ('{$profileName}', '{$name}|', '{$email}1{$config['escapeString']}', '{$newest[0]}','{$current_date}|','{$ip}|', '{$newest[1]}')";
+      $results = mysqli_query($conn, $sql);
+      if ($results == false) {
+        $to = 'admin@kieran.pw';
+        $subject = 'Error journal check';
+        $txt = __FILE__ . ' Error: ' . $sql . '<br>' . mysqli_error($conn);
+        $headers = 'From: fajournal@kieran.pw';
+        mail($to, $subject, $txt, $headers);
+        die("sql");
       }
     }
+
     $added .= ' ' . $profileName;
   }
-  else{
+  else {
     die("Added the following users to your monitor list:" . $added);
   }
 }
