830190cc9a86d4272de46b9a4af3ebfa39672ef6
Kieran <bmxstreet@gmx.net>
2018-04-16 09:52:35 +0200

    Update add.php

--- add.php
+++ add.php
@@ -6,7 +6,8 @@ function famon_add($errorMail, $conn, $username, $chatId, $profilesArr, $escapeS
   $ip = 'telegram';
   $current_date = date('Y-m-d H:i:s');
 
-  foreach ($profilesArr as $profileName) {
+  foreach ($profilesArr as $profileN) {
+    $profileName = strtolower($profileN);
     if (!empty($profileName)) {
       $sql = "SELECT `email` FROM `fajournalmon` WHERE `profile`='" . $profileName . "'";
       $results = mysqli_query($conn, $sql);
