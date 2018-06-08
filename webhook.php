9f545695b0c18ee08e98c5a774afbb9967ea956a
DatDraggy <spamme@kieran.pw>
2018-06-08 11:21:52 +0200

    Revert "Test"
    This reverts commit 06eb586dad2564ac79eee988e1b4bae06a1e7e1e.

--- webhook.php
+++ webhook.php
@@ -9,7 +9,7 @@ $chatId = sanitizeInputs($data['message']['chat']['id']);
 if(isset($data['message']['sticker'])){
   $sticker = $data['message']['sticker']['file_id'];
   $stickerUrl = getFile($sticker);
-  sendSticker($chatId, $stickerUrl);
+  sendSticker($chatId, $sticker);
   die();
 }
 
